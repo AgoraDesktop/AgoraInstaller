cdf09a765b48a787c3456c01b9be1281abf75995
Kyle J Cardoza <Kyle.Cardoza@icloud.com>
2023-09-07 21:48:11 -0400

    more scripting

--- install-agora-git.sh
+++ install-agora-git.sh
@@ -1,13 +1,42 @@
 #!/usr/local/bin/bash
 
-cd $(HOME)
+pushd $(HOME)
 mkdir -p Development/Agora
 cd Development/Agora
+
+# install prerequisites
+pkg install bash gmake cmake libffcall libxml2 libxslt openssl libiconv giflib aspell cups libaudiofile portaudio libart_lgpl windowmaker 
+
+# install gnustep-make
 git clone https://github.com/AgoraDesktop/tools-make.git
-cd tools-make
-./configure --enable-objc-arc --with-layout=agora
+pushd tools-make
+./configure --with-layout=agora
+gmake
 sudo gmake install && sudo gmake clean
-cd ..
+popd
+
+# Load the shell environment for gnustep-make
 . /System/Library/Makefiles/GNUstep.sh
+
+# install libobjc2
+git clone https://github.com/AgoraDesktop/libobjc2.git
+pushd libobjc2
+git submodule update --init
+mkdir Build
+cd Build 
+cmake -DGNUSTEP_INSTALL_TYPE=SYSTEM -DOLDABI_COMPAT=OFF ..
+gmake
+sudo gmake install
+cd ..
+rm -rf Build
+popd
+
+# install gnustep-base
 git clone https://github.com/AgoraDesktop/libs-base.git
-cd libs-base
+pushd libs-base
+./configure
+gmake -j8
+sudo gmake install
+gmake clean
+popd
+
